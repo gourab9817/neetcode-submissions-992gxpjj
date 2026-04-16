64633e5b057841023caa94e707846a71ebb80f94
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2026-04-17 00:41:34 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()<2)return false;
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            }else{
+                if(st.isEmpty()) return false;
+                    char top=st.pop();
+                    if(s.charAt(i)==')' && top!='(')return false;
+                    
+                    if(s.charAt(i)=='}' && top!='{')return false;
+                    
+                    if(s.charAt(i)==']' && top!='[')return false;
+                
+            }
+        }
+                
+        return st.isEmpty();
+    }
+}
